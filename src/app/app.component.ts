d3260ea6faacbbd02d88874bf644f049540a9559
Cooper Mayne <coopermayne@gmail.com>
2020-05-02 02:43:30 -1000

    got template and aos kind of working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
+}
